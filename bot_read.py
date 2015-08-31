73e414b0424c3f5ec28309e3f1a15118e08df66b
Nicholas Serra <serranic@users.noreply.github.com>
2015-08-31 14:36:48 -0400

    Update bot_read.py
    Changed to correct subreddit

--- bot_read.py
+++ bot_read.py
@@ -4,7 +4,7 @@ user_agent = ("PyEng Bot 0.1")
 
 r = praw.Reddit(user_agent = user_agent)
 
-subreddit = r.get_subreddit("learnpython")
+subreddit = r.get_subreddit("pythonforengineers")
 
 for submission in subreddit.get_hot(limit = 5):
     print "Title: ", submission.title
