0f464937f3472c4df7d09fc9c7e918c5c64af679
tobinjo <tobinjo@umich.edu>
2015-08-31 14:54:04 -0400

    Update firstattempt.py

--- firstattempt.py
+++ firstattempt.py
@@ -1,1 +1,8 @@
-
+import praw
+r = praw.Reddit(user_agent = 'John Tobin')
+subreddit = r.get_subreddit("learnpython")
+for submission in subreddit.get_hot(limit = 5):
+    print "Title: ", submission.title
+    print "Text: ", submission.selftext
+    print "Score: ", submission.score
+    print "---------------------------------\n"
