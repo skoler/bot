434f6f2570b1038efef640eb12714c2328ce5640
tobinjo <tobinjo@umich.edu>
2015-08-31 15:40:31 -0400

    Update firstattempt.py

--- firstattempt.py
+++ firstattempt.py
@@ -1,8 +1,16 @@
 import praw
+import pdb
+import re
+import os
+
+REDDIT_USERNAME = 'comeonspurs'
+REDDIT_PASSWORD = '305223'
 r = praw.Reddit(user_agent = 'John Tobin')
-subreddit = r.get_subreddit("learnpython")
-for submission in subreddit.get_hot(limit = 5):
-    print "Title: ", submission.title
-    print "Text: ", submission.selftext
-    print "Score: ", submission.score
-    print "---------------------------------\n"
+r.login(REDDIT_USERNAME, REDDIT_PASSWORD)
+subreddit = r.get_subreddit('pythonforengineers')
+for submission in subreddit.get_hot(limit=5):
+    titletext = submission.title
+    if titletext.find('fam') != -1:
+        submission.add_comment('Yo what''s good fam')
+    else:
+        print 'No fam in this title fam'
